61bf8e854b5f2d0ad7d2af9ba899271447ead5bc
Geonho <geonho@plantynet.com>
2022-01-11 11:10:40 +0900

    비밀번호 해쉬 후 회원가입하기

--- src/main/java/com/cos/blog/config/SecurityConfig.java
+++ src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.cos.blog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 //스프링 시큐리티 로그인페이지 커스터마이징을 위함
 
@@ -13,16 +15,22 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity(prePostEnabled = true) //특정 주소로 접근하면 권한/인증을 미리 체크하겠다는 뜻이다.
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+	@Bean //리턴하는 값을 빈으로 등록한다. 
+	public BCryptPasswordEncoder encodePWD() {
+		return new BCryptPasswordEncoder(); //객체 리턴해서 암호화
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+		.csrf().disable() //csrf토큰 비활성화 -> 테스트할 때 해놓는게 좋음
 			.authorizeRequests() //리퀘스트가 들어올 때 /auth/** 로 들어오는 애들 허용
-				.antMatchers("/auth/**")
+				.antMatchers("/","/auth/**","/js/**","/css/**","/image/**")
 				.permitAll()
 				.anyRequest()
-				.authenticated()
-			.and()
+				.authenticated()//나머지는 인증이 돼야함.
+			.and() //그리고
 				.formLogin()
-				.loginPage("/auth/loginForm"); //나머지는 인증이 돼야함.
+				.loginPage("/auth/loginForm"); //로그인페이지  
 	}
 }
--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -2,9 +2,11 @@ package com.cos.blog.service;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 
@@ -13,10 +15,16 @@ public class UserService {
 	
 	@Autowired
 	private UserRepository userRepository;
-	
+
+	@Autowired
+	private BCryptPasswordEncoder encoder;
 	
 	@Transactional //하나의 트랜잭션으로 묶여서 실행된다. 실행완료 -> commit, 실패 -> rollback
 	public void 회원가입(User user) {
+		String rawPassword = user. getPassword(); //원문
+		String encPassword = encoder.encode(rawPassword); //해쉬
+		user.setPassword(encPassword);
+		user.setRole(RoleType.USER);
 		userRepository.save(user);
 	}
 	
